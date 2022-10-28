241832a1f4952df9c9ceae0c6832fa9d07dad174
celeste062736 <skykimmg0627@gmail.com>
2022-10-28 16:56:27 +0900

    add

--- 1920/main.cpp
+++ 1920/main.cpp
@@ -1,28 +1,24 @@
 #include <cstdio>
+#include <map>
+using namespace std;
 
 int main()
 {
-    int n;
-    int temp;
-
+    int n, m,temp;
+    map<int, int> mp;
     scanf("%d", &n);
     for(int i = 0; i < n; i++)
     {
         scanf("%d", &temp);
-        data[temp] = 1;
+        auto iter = mp.insert(pair<int, int>(temp,1));
+        if(!iter.second)
+            iter.first->second++;
     }
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++)
+    scanf("%d", &m);
+    for(int i = 0; i < m; i++)
     {
         scanf("%d", &temp);
-        if(data[temp] == 1)
-        {
-            printf("1\n");
-        }
-        else
-        {
-            printf("0\n");
-        }
+        printf("%d", mp[temp]);
     }
     return 0;
 }
